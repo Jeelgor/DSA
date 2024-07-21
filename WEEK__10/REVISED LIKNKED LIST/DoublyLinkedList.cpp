ea2d1ca71dd47de8233cee93458c1e9b657ac8c4
Gor Jeel <jeelgor10@gmail.com>
2024-07-21 16:34:32 +0530

    dsa_practices

---
+++ WEEK__10/REVISED LIKNKED LIST/DoublyLinkedList.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+    Node()
+    {
+        this->prev = NULL;
+        this->next = NULL;
+    }
+
+    Node(int data)
+    {
+        this->data = data;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+void printLL(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+int findLen(Node *head)
+{
+    Node *temp = head;
+    int len = 0;
+    while (temp != NULL)
+    {
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+
+void InsertAtHead(Node *&head, Node *&tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        Node *newNode = new Node(data);
+        newNode->next = head;
+        head = head->prev;
+        head = newNode;
+    }
+}
+
+void InsertAtTail(Node *head, Node *tail, int data)
+{
+    if (head == NULL)
+    {
+        Node *newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+    }
+    else
+    {
+        Node *newNode = new Node(data);
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    InsertAtHead(head, tail, 40);
+    InsertAtHead(head, tail, 30);
+    InsertAtHead(head, tail, 20);
+    InsertAtHead(head, tail, 10);
+    printLL(head);
+    return 0;
+}
